0afa6bf90a0e400e3f623902eda518a4ce0bf95d
Vladymyr <92791503+HogHordGit@users.noreply.github.com>
2022-07-28 14:02:33 +0300

    Delete menu.js

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const btn = document.querySelector("#menu_ori_phone");
-const menu = document.querySelector("#menu");
-
-btn.addEventListener("click", () => {
-    menu.classList.toggle("disp");
-});
